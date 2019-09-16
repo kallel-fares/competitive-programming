187aa4a1d5217a705a40b639e251d07be60d52bf
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2019-09-16 10:51:09 +0100

    Solve gym100989A - LCS (A)
    Tags: none
    Problem: https://codeforces.com/gym/100989/problem/A
    Submission: https://codeforces.com/gym/100989/submission/60669365

---
+++ solutions/gym100989A-lcs-a.c
@@ -0,0 +1,27 @@
+/*
+ * Codeforces gym100989A - LCS (A)
+ * Rating: unrated | Tags: none
+ * Problem:    https://codeforces.com/gym/100989/problem/A
+ * Submission: https://codeforces.com/gym/100989/submission/60669365
+ * Solved: 2019-09-16 (VIRTUAL)
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+//srand(time(NULL));
+//nombre_aleatoire = rand();
+
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a==0)
+		printf("0");
+	else
+	
+		printf("%d",a+1);
+}
