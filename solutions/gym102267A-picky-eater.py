351d7570b0d9b8c36783b120d35a355d7f8c3265
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2019-10-07 10:27:03 +0100

    Solve gym102267A - Picky Eater
    Tags: none
    Problem: https://codeforces.com/gym/102267/problem/A
    Submission: https://codeforces.com/gym/102267/submission/62051079

---
+++ solutions/gym102267A-picky-eater.py
@@ -0,0 +1,11 @@
+# Codeforces gym102267A - Picky Eater
+# Rating: unrated | Tags: none
+# Problem:    https://codeforces.com/gym/102267/problem/A
+# Submission: https://codeforces.com/gym/102267/submission/62051079
+# Solved: 2019-10-07 (PRACTICE)
+
+l=list(map(int,input().split()))
+if l[0]>=l[1]:
+    print("1")
+else:
+    print("0")
