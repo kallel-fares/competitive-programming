773b9cde1eb0e84348409a71952a29b4cba654ba
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-04-01 22:29:35 +0100

    Solve 1331A - Is it rated?
    Tags: *special
    Problem: https://codeforces.com/problemset/problem/1331/A
    Submission: https://codeforces.com/contest/1331/submission/75183774

---
+++ solutions/1331A-is-it-rated.cpp
@@ -0,0 +1,29 @@
+/*
+ * Codeforces 1331A - Is it rated?
+ * Rating: unrated | Tags: *special
+ * Problem:    https://codeforces.com/problemset/problem/1331/A
+ * Submission: https://codeforces.com/contest/1331/submission/75183774
+ * Solved: 2020-04-01 (PRACTICE)
+ */
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <time.h>
+
+//srand(time(NULL));
+//nombre_aleatoire = rand();
+
+#define pb push_back
+#define ll long long
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	printf("NO");
+
+
+}
