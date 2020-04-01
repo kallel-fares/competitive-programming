7582756d4baf1786ee287091b0d86f361774dac7
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-04-01 22:55:40 +0100

    Solve 1331D - Again?
    Tags: *special, implementation
    Problem: https://codeforces.com/problemset/problem/1331/D
    Submission: https://codeforces.com/contest/1331/submission/75185135

---
+++ solutions/1331D-again.cpp
@@ -0,0 +1,35 @@
+/*
+ * Codeforces 1331D - Again?
+ * Rating: unrated | Tags: *special, implementation
+ * Problem:    https://codeforces.com/problemset/problem/1331/D
+ * Submission: https://codeforces.com/contest/1331/submission/75185135
+ * Solved: 2020-04-01 (PRACTICE)
+ */
+
+//#include <stdio.h>
+//#include <math.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <time.h>
+
+//srand(time(NULL));
+//nombre_aleatoire = rand();
+
+#define pb push_back
+#define ll long long
+
+#include <bits/stdc++.h>
+using namespace std;
+char ch[8];
+int main ()
+{
+	scanf("%s",ch);
+	if((int)ch[6]&1)
+	{
+		printf("1");
+	}
+	else
+		printf("0");
+
+
+}
