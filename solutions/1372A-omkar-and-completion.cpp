75b8c093a0bfcd8162624342ecbfd89d82484c66
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-07-11 16:36:00 +0100

    Solve 1372A - Omkar and Completion (800)
    Tags: constructive algorithms, implementation
    Problem: https://codeforces.com/problemset/problem/1372/A
    Submission: https://codeforces.com/contest/1372/submission/86550197

---
+++ solutions/1372A-omkar-and-completion.cpp
@@ -0,0 +1,35 @@
+/*
+ * Codeforces 1372A - Omkar and Completion
+ * Rating: 800 | Tags: constructive algorithms, implementation
+ * Problem:    https://codeforces.com/problemset/problem/1372/A
+ * Submission: https://codeforces.com/contest/1372/submission/86550197
+ * Solved: 2020-07-11 (CONTESTANT)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	testc
+	{
+		cin>>n;
+		for(int i=0;i<n;i++)
+		{
+			cout<<"1 ";
+		}
+		cout<<"\n";
+	}
+
+
+}
