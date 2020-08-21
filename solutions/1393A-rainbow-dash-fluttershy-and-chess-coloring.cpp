cd96381038a47933ca4d9d84a289d7e7fd343fca
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-08-21 16:31:46 +0100

    Solve 1393A - Rainbow Dash, Fluttershy and Chess Coloring (800)
    Tags: greedy, math
    Problem: https://codeforces.com/problemset/problem/1393/A
    Submission: https://codeforces.com/contest/1393/submission/90584826

---
+++ solutions/1393A-rainbow-dash-fluttershy-and-chess-coloring.cpp
@@ -0,0 +1,32 @@
+/*
+ * Codeforces 1393A - Rainbow Dash, Fluttershy and Chess Coloring
+ * Rating: 800 | Tags: greedy, math
+ * Problem:    https://codeforces.com/problemset/problem/1393/A
+ * Submission: https://codeforces.com/contest/1393/submission/90584826
+ * Solved: 2020-08-21 (PRACTICE)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	testc
+	{
+		int n;
+		cin>>n;
+		cout<<n/2+1<<"\n";
+	}
+
+}
