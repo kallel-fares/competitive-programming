c2fb397b6ff91d1b1f3a7130c8c5fc2a81c62e6a
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-10-01 22:27:11 +0100

    Solve 1417A - Copy-paste (800)
    Tags: greedy, math
    Problem: https://codeforces.com/problemset/problem/1417/A
    Submission: https://codeforces.com/contest/1417/submission/94441543

---
+++ solutions/1417A-copy-paste.cpp
@@ -0,0 +1,44 @@
+/*
+ * Codeforces 1417A - Copy-paste
+ * Rating: 800 | Tags: greedy, math
+ * Problem:    https://codeforces.com/problemset/problem/1417/A
+ * Submission: https://codeforces.com/contest/1417/submission/94441543
+ * Solved: 2020-10-01 (VIRTUAL)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+
+const int N=2e5+5;
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		int n,k,tab[N]={0},s=0;
+		cin>>n>>k;
+		for(int i=0;i<n;i++)
+		{
+			cin>>tab[i];
+		}
+		sort(tab,tab+n);
+		
+		for(int i=1;i<n;i++)
+		{
+			s+=(k-tab[i])/tab[0];
+		}
+		cout<<s<<"\n";
+		
+		
+	}
+
+}
