4fb1ee25ccc05f295bd11e71cf2607d8c3f6c6e1
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-10-12 10:39:16 +0100

    Solve 1422A - Fence (800)
    Tags: geometry, math
    Problem: https://codeforces.com/problemset/problem/1422/A
    Submission: https://codeforces.com/contest/1422/submission/95329747

---
+++ solutions/1422A-fence.cpp
@@ -0,0 +1,30 @@
+/*
+ * Codeforces 1422A - Fence
+ * Rating: 800 | Tags: geometry, math
+ * Problem:    https://codeforces.com/problemset/problem/1422/A
+ * Submission: https://codeforces.com/contest/1422/submission/95329747
+ * Solved: 2020-10-12 (VIRTUAL)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		ll a,b,c;
+		cin>>a>>b>>c;
+		cout<<a+b+c-2<<"\n";
+	}
+
+}
