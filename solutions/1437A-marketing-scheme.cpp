6b0dbb3347a0fe7488fddfc2647fb69495b3afbd
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-10-27 21:08:46 +0100

    Solve 1437A - Marketing Scheme (800)
    Tags: brute force, constructive algorithms, greedy, math
    Problem: https://codeforces.com/problemset/problem/1437/A
    Submission: https://codeforces.com/contest/1437/submission/96940359

---
+++ solutions/1437A-marketing-scheme.cpp
@@ -0,0 +1,38 @@
+/*
+ * Codeforces 1437A - Marketing Scheme
+ * Rating: 800 | Tags: brute force, constructive algorithms, greedy, math
+ * Problem:    https://codeforces.com/problemset/problem/1437/A
+ * Submission: https://codeforces.com/contest/1437/submission/96940359
+ * Solved: 2020-10-27 (VIRTUAL)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		int l,r;
+		cin>>l>>r;
+		if(l*2>r)
+		{
+			cout<<"YES\n";
+		}
+		else
+			cout<<"NO\n";
+	}
+
+
+}
