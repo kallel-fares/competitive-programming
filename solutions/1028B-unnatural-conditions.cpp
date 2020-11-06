7360f734e8b23cda3014b3173242dc4aee041eee
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-11-06 10:33:22 +0100

    Solve 1028B - Unnatural Conditions (1200)
    Tags: constructive algorithms, math
    Problem: https://codeforces.com/problemset/problem/1028/B
    Submission: https://codeforces.com/contest/1028/submission/97765506

---
+++ solutions/1028B-unnatural-conditions.cpp
@@ -0,0 +1,41 @@
+/*
+ * Codeforces 1028B - Unnatural Conditions
+ * Rating: 1200 | Tags: constructive algorithms, math
+ * Problem:    https://codeforces.com/problemset/problem/1028/B
+ * Submission: https://codeforces.com/contest/1028/submission/97765506
+ * Solved: 2020-11-06 (PRACTICE)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int s=0,s1=0;
+	string ch,ch1;
+	
+	cin>>ch>>ch1;
+	for(int i=0;i<2230;i++)
+		cout<<'5';
+	cout<<"\n";
+	for(int i=0;i<2229;i++)
+	{
+		cout<<"4";
+	}
+	cout<<"5";
+	
+	
+	
+}
