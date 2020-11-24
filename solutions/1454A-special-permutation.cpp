cb05278e2ba0ccc4e7ec81b0fe7cc76c1f0e54d7
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-11-24 15:40:24 +0100

    Solve 1454A - Special Permutation (800)
    Tags: constructive algorithms, probabilities
    Problem: https://codeforces.com/problemset/problem/1454/A
    Submission: https://codeforces.com/contest/1454/submission/99414037

---
+++ solutions/1454A-special-permutation.cpp
@@ -0,0 +1,37 @@
+/*
+ * Codeforces 1454A - Special Permutation
+ * Rating: 800 | Tags: constructive algorithms, probabilities
+ * Problem:    https://codeforces.com/problemset/problem/1454/A
+ * Submission: https://codeforces.com/contest/1454/submission/99414037
+ * Solved: 2020-11-24 (CONTESTANT)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		int n;
+		cin>>n;
+		for(int i=0;i<n-1;i++)
+		{
+			cout<<i+2<<" ";
+		}
+		cout<<"1\n";
+	}
+
+
+}
