9666c6ca13de58839091f653309e5a9d98208fb8
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-11-30 16:02:09 +0100

    Solve 1455C - Ping-pong (1100)
    Tags: constructive algorithms, games, math
    Problem: https://codeforces.com/problemset/problem/1455/C
    Submission: https://codeforces.com/contest/1455/submission/100010956

---
+++ solutions/1455C-ping-pong.cpp
@@ -0,0 +1,33 @@
+/*
+ * Codeforces 1455C - Ping-pong
+ * Rating: 1100 | Tags: constructive algorithms, games, math
+ * Problem:    https://codeforces.com/problemset/problem/1455/C
+ * Submission: https://codeforces.com/contest/1455/submission/100010956
+ * Solved: 2020-11-30 (CONTESTANT)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		int x,y;
+		cin>>x>>y;
+		cout<<x-1<<" "<<y<<"\n";
+	}
+
+
+}
