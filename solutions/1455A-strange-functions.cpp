2b00062416d711f01cf7892a164f8207e222d1f7
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-11-30 15:40:51 +0100

    Solve 1455A - Strange Functions (800)
    Tags: math, number theory
    Problem: https://codeforces.com/problemset/problem/1455/A
    Submission: https://codeforces.com/contest/1455/submission/99998800

---
+++ solutions/1455A-strange-functions.cpp
@@ -0,0 +1,34 @@
+/*
+ * Codeforces 1455A - Strange Functions
+ * Rating: 800 | Tags: math, number theory
+ * Problem:    https://codeforces.com/problemset/problem/1455/A
+ * Submission: https://codeforces.com/contest/1455/submission/99998800
+ * Solved: 2020-11-30 (CONTESTANT)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		string ch;
+		cin>>ch;
+		cout<<ch.size()<<"\n";
+	
+	}
+
+
+}
