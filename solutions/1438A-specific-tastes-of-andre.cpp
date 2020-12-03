a619760392cf99810bad17caa50ae33f50d12ba6
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2020-12-03 18:44:58 +0100

    Solve 1438A - Specific Tastes of Andre  (800)
    Tags: constructive algorithms, implementation
    Problem: https://codeforces.com/problemset/problem/1438/A
    Submission: https://codeforces.com/contest/1438/submission/100295968

---
+++ solutions/1438A-specific-tastes-of-andre.cpp
@@ -0,0 +1,34 @@
+/*
+ * Codeforces 1438A - Specific Tastes of Andre 
+ * Rating: 800 | Tags: constructive algorithms, implementation
+ * Problem:    https://codeforces.com/problemset/problem/1438/A
+ * Submission: https://codeforces.com/contest/1438/submission/100295968
+ * Solved: 2020-12-03 (VIRTUAL)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	testc
+	{
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++)cout<<"1 ";
+		cout<<"\n";
+	}
+
+
+}
