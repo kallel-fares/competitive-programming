2d95d6091930dd35a2fa91864b2d6f3ffd9d7f25
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-02-27 14:10:44 +0100

    Solve 1421C - Palindromifier (1400)
    Tags: constructive algorithms, strings
    Problem: https://codeforces.com/problemset/problem/1421/C
    Submission: https://codeforces.com/contest/1421/submission/108608035

---
+++ solutions/1421C-palindromifier.cpp
@@ -0,0 +1,36 @@
+/*
+ * Codeforces 1421C - Palindromifier
+ * Rating: 1400 | Tags: constructive algorithms, strings
+ * Problem:    https://codeforces.com/problemset/problem/1421/C
+ * Submission: https://codeforces.com/contest/1421/submission/108608035
+ * Solved: 2021-02-27 (PRACTICE)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//freopen("","r",stdin);
+	string ch;
+	cin>>ch;
+	int len=ch.size();
+	cout<<"3\n";
+	cout<<"R "<<len-1<<"\n";
+	cout<<"L "<<len<<"\n";
+	cout<<"L 2\n";
+
+
+}
