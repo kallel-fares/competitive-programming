b1088fe43575e1daaf744f4aaad881c1d13356e2
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-03-10 14:59:23 +0100

    Solve 1496C - Diamond Miner (1200)
    Tags: geometry, greedy, sortings
    Problem: https://codeforces.com/problemset/problem/1496/C
    Submission: https://codeforces.com/contest/1496/submission/109619813

---
+++ solutions/1496C-diamond-miner.cpp
@@ -0,0 +1,59 @@
+/*
+ * Codeforces 1496C - Diamond Miner
+ * Rating: 1200 | Tags: geometry, greedy, sortings
+ * Problem:    https://codeforces.com/problemset/problem/1496/C
+ * Submission: https://codeforces.com/contest/1496/submission/109619813
+ * Solved: 2021-03-10 (CONTESTANT)
+ */
+
+/***********dicfore************/
+
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=3e5+5;
+
+int main ()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	//freopen("","r",stdin);
+	testc
+	{
+		
+		int n;
+		cin>>n;
+		vector<ll> vec,vec2;
+		double ans=0;
+		
+		for(int i=0;i<2*n;i++)
+		{
+			ll a,b;
+			cin>>a>>b;
+			if(!a)
+				vec.pb(abs(b));
+			else
+				vec2.pb(abs(a));
+			
+		}
+		
+			
+		sort(vec.begin(),vec.end());
+		sort(vec2.begin(),vec2.end());
+	
+		for(int i=0;i<n;i++)
+		{
+			ans+=sqrt(vec[i]*vec[i]+vec2[i]*vec2[i]);
+		}
+		
+    	cout << setprecision(20) << ans<<"\n";
+	}
+
+
+}
