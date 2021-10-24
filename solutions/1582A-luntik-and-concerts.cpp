4aef8f6b22afc684ae7abbd09e211860a9cda317
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-10-24 11:19:16 +0100

    Solve 1582A - Luntik and Concerts (800)
    Tags: math
    Problem: https://codeforces.com/problemset/problem/1582/A
    Submission: https://codeforces.com/contest/1582/submission/132862270

---
+++ solutions/1582A-luntik-and-concerts.cpp
@@ -0,0 +1,49 @@
+/*
+ * Codeforces 1582A - Luntik and Concerts
+ * Rating: 800 | Tags: math
+ * Problem:    https://codeforces.com/problemset/problem/1582/A
+ * Submission: https://codeforces.com/contest/1582/submission/132862270
+ * Solved: 2021-10-24 (CONTESTANT)
+ */
+
+/***********dicfore************/
+ 
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+ 
+const int N=3e5+5;
+ 
+ 
+ 
+int main ()
+{
+    
+    
+    testc
+    {
+        ll a,b,c;
+        cin>>a>>b>>c;
+        
+        
+        cout<<(a+b*2+c*3)%2<<"\n";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+}
