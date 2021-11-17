5d94d5eeb4eb136bcabe0c0a29c63cbd45de88c7
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-11-17 17:28:32 +0100

    Solve 1606A - AB Balance (900)
    Tags: strings
    Problem: https://codeforces.com/problemset/problem/1606/A
    Submission: https://codeforces.com/contest/1606/submission/135998690

---
+++ solutions/1606A-ab-balance.cpp
@@ -0,0 +1,45 @@
+/*
+ * Codeforces 1606A - AB Balance
+ * Rating: 900 | Tags: strings
+ * Problem:    https://codeforces.com/problemset/problem/1606/A
+ * Submission: https://codeforces.com/contest/1606/submission/135998690
+ * Solved: 2021-11-17 (VIRTUAL)
+ */
+
+/***********dicfore************/
+ 
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+ 
+
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+
+    
+    testc
+    {
+        
+        string ch;
+        cin>>ch;
+        
+        if(ch[0]!=ch[ch.size()-1])
+            ch[0]=ch[ch.size()-1];
+        cout<<ch<<"\n";
+        
+        
+        
+    }
+    
+    
+}
