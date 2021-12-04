3a65ae17c18c974d6d24383277132945e3d93938
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-12-04 16:59:04 +0100

    Solve 1605A - A.M. Deviation (800)
    Tags: math, number theory
    Problem: https://codeforces.com/problemset/problem/1605/A
    Submission: https://codeforces.com/contest/1605/submission/138007521

---
+++ solutions/1605A-a-m-deviation.cpp
@@ -0,0 +1,61 @@
+/*
+ * Codeforces 1605A - A.M. Deviation
+ * Rating: 800 | Tags: math, number theory
+ * Problem:    https://codeforces.com/problemset/problem/1605/A
+ * Submission: https://codeforces.com/contest/1605/submission/138007521
+ * Solved: 2021-12-04 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+
+
+const int m=1e9+7;
+ 
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    testc{
+        int a,b,c;
+        cin>>a>>b>>c;
+        
+        if((a+c-b*2)%3==0)
+            cout<<"0";
+        else
+            cout<<"1";
+        
+        cout<<"\n";
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
+        
+    
+    
+    
+}
