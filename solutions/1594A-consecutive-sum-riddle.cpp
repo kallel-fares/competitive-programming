2e3c555e773adc07bd302d7ac02bcbc9b609896b
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-12-18 22:59:48 +0100

    Solve 1594A - Consecutive Sum Riddle (800)
    Tags: math
    Problem: https://codeforces.com/problemset/problem/1594/A
    Submission: https://codeforces.com/contest/1594/submission/139842323

---
+++ solutions/1594A-consecutive-sum-riddle.cpp
@@ -0,0 +1,48 @@
+/*
+ * Codeforces 1594A - Consecutive Sum Riddle
+ * Rating: 800 | Tags: math
+ * Problem:    https://codeforces.com/problemset/problem/1594/A
+ * Submission: https://codeforces.com/contest/1594/submission/139842323
+ * Solved: 2021-12-18 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+
+
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+
+
+
+const int m=1e9+7;
+
+
+queue<int> q;
+
+
+
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    testc{
+        ll n;
+        cin>>n;
+        
+        cout<<-(n-1)<<" "<<n<<"\n";
+        
+        
+    }
+    
+
+
+}
