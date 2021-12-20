b45b45951446c68456d76861b28f8375d0f9c053
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2021-12-20 12:09:38 +0100

    Solve 1538A - Stone Game (800)
    Tags: brute force, dp, greedy
    Problem: https://codeforces.com/problemset/problem/1538/A
    Submission: https://codeforces.com/contest/1538/submission/139998657

---
+++ solutions/1538A-stone-game.cpp
@@ -0,0 +1,109 @@
+/*
+ * Codeforces 1538A - Stone Game
+ * Rating: 800 | Tags: brute force, dp, greedy
+ * Problem:    https://codeforces.com/problemset/problem/1538/A
+ * Submission: https://codeforces.com/contest/1538/submission/139998657
+ * Solved: 2021-12-20 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-9
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+
+
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+
+
+
+const int m=1e9+7;
+
+
+
+queue<int> q;
+
+
+
+
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    testc{
+       
+        int n;
+        cin>>n;
+        vector<int> vec(n);
+        
+        
+        for(int i=0;i<n;i++)
+            cin>>vec[i];
+        
+        
+        
+        int v=0,cnt=0,ans=500;
+        for(int i=0;i<n;i++)
+        {
+            cnt++;
+            if(vec[i]==1 || vec[i]==n)
+                v++;
+            
+            if(v==2)
+                break;
+        }
+        ans=min(ans,cnt);
+        
+        v=0;
+        cnt=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            cnt++;
+            if(vec[i]==1 || vec[i]==n)
+                v++;
+            
+            if(v==2)
+                break;
+        }
+        ans=min(ans,cnt);
+        
+        
+        v=0;
+        cnt=0;
+        int i=0,j=n-1;
+        
+        
+        while(v<2)
+        {
+            if(!v)
+                cnt+=2;
+            else
+                cnt++;
+            if(vec[i]==1 || vec[i]==n)
+            {
+                v++;
+                
+            }
+            
+            if(vec[j]==n || vec[j]==1)
+            {
+                v++;
+            }
+            i++;
+            j--;
+            
+        }
+        
+        ans=min(ans,cnt);
+        cout<<ans<<"\n";
+        //11101111111110
+    }
+    
+
+
+}
