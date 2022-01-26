a78f419fd3909f5bcc76b4c830592ff5266b9cb4
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-01-26 11:07:37 +0100

    Solve 1549A - Gregor and Cryptography (800)
    Tags: math, number theory
    Problem: https://codeforces.com/problemset/problem/1549/A
    Submission: https://codeforces.com/contest/1549/submission/144055601

---
+++ solutions/1549A-gregor-and-cryptography.cpp
@@ -0,0 +1,58 @@
+/*
+ * Codeforces 1549A - Gregor and Cryptography
+ * Rating: 800 | Tags: math, number theory
+ * Problem:    https://codeforces.com/problemset/problem/1549/A
+ * Submission: https://codeforces.com/contest/1549/submission/144055601
+ * Solved: 2022-01-26 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+//#include "/users/kallelfares/bits/stdc++.h"
+using namespace std;
+ 
+ 
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+ 
+ 
+ 
+const int m=1e9+7;
+
+ 
+ 
+ 
+/*
+ 
+ 
+ 
+ 2 1 3
+ 
+ 
+ 
+*/
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+    testc
+    {
+        
+        
+        int a;
+        cin>>a;
+        
+        
+        cout<<"2 "<<a-1<<"\n";
+    }
+        
+        
+     
+}
