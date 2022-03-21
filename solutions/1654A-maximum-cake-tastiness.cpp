a7727a67b0236d562e3301ab29375af6b4c2fb9e
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-03-21 09:39:31 +0100

    Solve 1654A - Maximum Cake Tastiness (800)
    Tags: brute force, greedy, implementation, sortings
    Problem: https://codeforces.com/problemset/problem/1654/A
    Submission: https://codeforces.com/contest/1654/submission/150337156

---
+++ solutions/1654A-maximum-cake-tastiness.cpp
@@ -0,0 +1,68 @@
+/*
+ * Codeforces 1654A - Maximum Cake Tastiness
+ * Rating: 800 | Tags: brute force, greedy, implementation, sortings
+ * Problem:    https://codeforces.com/problemset/problem/1654/A
+ * Submission: https://codeforces.com/contest/1654/submission/150337156
+ * Solved: 2022-03-21 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+
+
+const int m=1e9+7;
+
+
+ll gcd(ll a, ll b){
+    if (b == 0)
+       return a;
+    return gcd(b, a % b);
+}
+
+
+ll pop(const ll n)
+{
+    ll ans=0;
+    for(ll i=0;i<=40;i++)
+    {
+        if((1LL<<i)&n)
+            ans++;
+    }
+    return ans;
+}
+ 
+ 
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+    testc
+    {
+        int n;
+        cin>>n;
+        vector<int> vec(n);
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>vec[i];
+        }
+        
+        sort(vec.begin(), vec.end());
+        
+        cout<<vec[n-1]+vec[n-2]<<"\n";
+    }
+    
+    
+}
