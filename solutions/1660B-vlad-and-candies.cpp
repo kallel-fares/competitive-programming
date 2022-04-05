67b293246e85855dd513491d5c8d498bff160f50
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-04-05 20:56:45 +0100

    Solve 1660B - Vlad and Candies (800)
    Tags: math
    Problem: https://codeforces.com/problemset/problem/1660/B
    Submission: https://codeforces.com/contest/1660/submission/152794553

---
+++ solutions/1660B-vlad-and-candies.cpp
@@ -0,0 +1,104 @@
+/*
+ * Codeforces 1660B - Vlad and Candies
+ * Rating: 800 | Tags: math
+ * Problem:    https://codeforces.com/problemset/problem/1660/B
+ * Submission: https://codeforces.com/contest/1660/submission/152794553
+ * Solved: 2022-04-05 (VIRTUAL)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+
+
+//const int m=1e9+7;
+
+
+ll gcd(ll a, ll b){
+    if (b == 0)
+       return a;
+    return gcd(b, a % b);
+}
+
+ll ans;
+
+ll dfs(vector<vector<int>> & tree, int a,vector<ll> &vec)
+{
+    ll mini=1e9+5;
+    //cout<<a<<" |";
+    if(!tree[a].size())
+    {
+        
+            return vec[a];
+    }
+        
+    for(auto x:tree[a])
+    {
+        ll num=dfs(tree,x,vec);
+        //cout<<num<<" "<<x<<"|"<<ans<<" *";
+        mini=min(mini,num);
+        ans+=num;
+    }
+    
+    //cout<<ans<<" ";
+    
+    ll aux=max(mini,vec[a]);
+    ans+=-mini;
+    //cout<<ans<<" ";
+    return aux;
+    
+}
+ 
+ 
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+    
+    testc{
+        
+        int n;
+        cin>>n;
+        
+        vector<int>vec(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            cin>>vec[i];
+        }
+        
+        if(n==1 && vec[0]>1)
+        {
+            cout<<"NO\n";
+            continue;
+        }
+        
+        sort(vec.begin(), vec.end(),greater<int>());
+        
+        int v=0;
+        if(vec[0]-vec[1]<=1)
+        {
+            
+                cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+        
+    }
+    
+    
+    
+    
+}
