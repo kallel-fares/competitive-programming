967b99c890689ba04e60891825efb0f2c4bd0f9a
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-04-21 16:22:11 +0100

    Solve 1669E - 2-Letter Strings (1200)
    Tags: data structures, math, strings
    Problem: https://codeforces.com/problemset/problem/1669/E
    Submission: https://codeforces.com/contest/1669/submission/154362388

---
+++ solutions/1669E-2-letter-strings.cpp
@@ -0,0 +1,121 @@
+/*
+ * Codeforces 1669E - 2-Letter Strings
+ * Rating: 1200 | Tags: data structures, math, strings
+ * Problem:    https://codeforces.com/problemset/problem/1669/E
+ * Submission: https://codeforces.com/contest/1669/submission/154362388
+ * Solved: 2022-04-21 (OUT_OF_COMPETITION)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+
+
+const int m=1e9+7;
+
+
+bool cmp(pair<int, int>a,pair<int, int>b){
+    
+    if(a.second==b.second) return a.first<b.first;
+    return a.second<b.second;
+    
+}
+ 
+
+
+
+vector<ll> spec,vals;
+
+
+pair<ll, ll> dfs(vector<vector<int>>& vec,int u,int v)
+{
+//    cout<<u<<" ";
+    
+    ll val=0;
+    
+    if(spec[u])
+    {
+        val=vals[u];
+    }
+    
+    pair<ll, ll> ans;
+    
+    for(auto x:vec[u])
+    {
+        if(x==v)
+            continue;
+        
+        pair<ll, ll> aux=dfs(vec,x,u);
+        
+        ans.second=max(ans.second,aux.second);
+        ans.first+=aux.first;
+        
+    }
+    
+    //cout<<u<<" "<<ans.first<<" "<<ans.second<<"\n";
+    
+    if(ans.first)
+    {
+        val=vals[u];
+        if(val>=ans.second)
+        {
+            ans.first+=val-ans.second;
+            ans.second=val;
+        }
+            
+    }
+    else
+    {
+        ans.first=val;
+        ans.second=val;
+    }
+        
+        
+    return ans;
+    
+}
+
+
+int main ()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    testc
+    {
+        int n;
+        cin>>n;
+        map<string, ll> m;
+        
+        for(int i=0;i<n;i++)
+        {
+            string aux;
+            cin>>aux;
+            m[aux]++;
+        }
+        
+        ll ans=0;
+        
+        vector<ll> freq1(50),freq2(50);
+        for(auto x:m)
+        {
+            //cout<<x.first<<" "<<x.second<<"\n";
+            //cout<<x.second*freq[x.first[0]-'a']+x.second*freq[x.first[1]-'a']<<" ";
+            ans+=x.second*freq1[x.first[0]-'a']+x.second*freq2[x.first[1]-'a'];
+            freq1[x.first[0]-'a']+=x.second;
+            freq2[x.first[1]-'a']+=x.second;
+        }
+        
+        cout<<ans<<"\n";
+    }
+    
+}
