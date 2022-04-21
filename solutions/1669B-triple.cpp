78c042d7177ed7e38bad92d48791ded179261103
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-04-21 15:42:10 +0100

    Solve 1669B - Triple (800)
    Tags: implementation, sortings
    Problem: https://codeforces.com/problemset/problem/1669/B
    Submission: https://codeforces.com/contest/1669/submission/154306561

---
+++ solutions/1669B-triple.cpp
@@ -0,0 +1,118 @@
+/*
+ * Codeforces 1669B - Triple
+ * Rating: 800 | Tags: implementation, sortings
+ * Problem:    https://codeforces.com/problemset/problem/1669/B
+ * Submission: https://codeforces.com/contest/1669/submission/154306561
+ * Solved: 2022-04-21 (OUT_OF_COMPETITION)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+
+
+const int m=1e9+7;
+
+
+bool cmp(pair<int, int>a,pair<int, int>b){
+    
+    if(a.second==b.second) return a.first<b.first;
+    return a.second<b.second;
+    
+}
+ 
+
+
+
+vector<ll> spec,vals;
+
+
+pair<ll, ll> dfs(vector<vector<int>>& vec,int u,int v)
+{
+//    cout<<u<<" ";
+    
+    ll val=0;
+    
+    if(spec[u])
+    {
+        val=vals[u];
+    }
+    
+    pair<ll, ll> ans;
+    
+    for(auto x:vec[u])
+    {
+        if(x==v)
+            continue;
+        
+        pair<ll, ll> aux=dfs(vec,x,u);
+        
+        ans.second=max(ans.second,aux.second);
+        ans.first+=aux.first;
+        
+    }
+    
+    //cout<<u<<" "<<ans.first<<" "<<ans.second<<"\n";
+    
+    if(ans.first)
+    {
+        val=vals[u];
+        if(val>=ans.second)
+        {
+            ans.first+=val-ans.second;
+            ans.second=val;
+        }
+            
+    }
+    else
+    {
+        ans.first=val;
+        ans.second=val;
+    }
+        
+        
+    return ans;
+    
+}
+
+
+int main ()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    testc
+    {
+        int n;
+        cin>>n;
+
+        vector<int> vec(n+1);
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            vec[a]++;
+            if(vec[a]>=3)
+                ans=a;
+        }
+        
+        
+        if(ans==0)
+            cout<<"-1\n";
+        else
+            cout<<ans<<"\n";
+        
+    }
+    
+}
