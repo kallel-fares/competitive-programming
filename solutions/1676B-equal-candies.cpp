b9137d833b8e10e7e8c1f921e391590e19eaa32d
Fares Kallel <83583846+kallel-fares@users.noreply.github.com>
2022-05-10 15:50:16 +0100

    Solve 1676B - Equal Candies (800)
    Tags: greedy, math, sortings
    Problem: https://codeforces.com/problemset/problem/1676/B
    Submission: https://codeforces.com/contest/1676/submission/156633701

---
+++ solutions/1676B-equal-candies.cpp
@@ -0,0 +1,94 @@
+/*
+ * Codeforces 1676B - Equal Candies
+ * Rating: 800 | Tags: greedy, math, sortings
+ * Problem:    https://codeforces.com/problemset/problem/1676/B
+ * Submission: https://codeforces.com/contest/1676/submission/156633701
+ * Solved: 2022-05-10 (CONTESTANT)
+ */
+
+/*********dicfore************/
+#define pb push_back
+#define ll long long
+#define lowpos 1e-4
+#define forl(i,b,n) for(int i=b;i<n;i++)
+#define testc int t;cin>>t;while(t--)
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+
+
+const int m=1e9+7;
+
+
+ll gcd(ll a, ll b){
+    if (b == 0)
+       return a;
+    return gcd(b, a % b);
+}
+
+//
+//ll dfs(vector<vector<int>> & tree, int a,vector<ll> &vec)
+//{
+//    ll mini=1e9+5;
+//    //cout<<a<<" |";
+//    if(!tree[a].size())
+//    {
+//
+//            return vec[a];
+//    }
+//
+//    for(auto x:tree[a])
+//    {
+//        ll num=dfs(tree,x,vec);
+//        mini=min(mini,num);
+//        ans+=num;
+//    }
+//
+//    ll aux=max(mini,vec[a]);
+//    ans+=-mini;
+//    return aux;
+//
+//}
+
+
+
+
+ 
+ 
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+
+    
+    testc
+    {
+        int n;
+        cin>>n;
+        vector<int> vec(n);
+        
+        int mini=1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>vec[i];
+            mini=min(vec[i],mini);
+        }
+        
+        ll ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=vec[i]-mini;
+        }
+        cout<<ans<<"\n";
+        
+        
+        
+    }
+    
+    
+    
+}
